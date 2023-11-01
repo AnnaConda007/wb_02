e7e40e66881ae52c2a4c352fa9af5406c4d3a917
Anna Hrulckova <annahrulkova@yandex.ru>
2023-11-01 13:59:13 +0400

    сортировка

--- 24/getData.js
+++ 24/getData.js
@@ -3,7 +3,6 @@ const getData = async () => {
     "http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true";
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 };
 
--- 24/renderData.js
+++ 24/renderData.js
@@ -1,69 +1,66 @@
 import getData from "./getData.js";
 
 const renderData = async () => {
-   const tableBody = document.querySelector("table tbody");
-  const paginateStep = 50;
-  let currentStep = 0;
-  let currentSortType = "";
+  const tableBody = document.querySelector("tbody");
+  const btn = document.querySelector("button");
+  const paginateStep = 5; // 5 нагляднее чем 50
+  let currentStep = paginateStep;
+  let currentSortColumn = "";
   let currentSortOrder = 1;
-  
   const data = await getData();
+
   const sortData = (data, type) => {
-    if (!type) return data;
     return data.sort((a, b) => {
       if (a[type] > b[type]) return currentSortOrder;
       if (a[type] < b[type]) return -currentSortOrder;
-      return 0;
     });
   };
-
-  const displayData = (dataToDisplay) => {
-    const ren = dataToDisplay
+  const displayData = (dataToDisplay, append = false) => {
+    const rendered = dataToDisplay
       .map(
         (str) => `
-        <tr> 
-          <td>${str.address}</td>
-          <td>${str.city}</td>
-          <td>${str.fname}</td>
-          <td>${str.lname}</td>
-          <td>${str.state}</td>
-          <td>${str.tel}</td>
-          <td>${str.zip}</td>
-        </tr>
-      `
+      <tr> 
+        <td>${str.address}</td>
+        <td>${str.city}</td>
+        <td>${str.fname}</td>
+        <td>${str.lname}</td>
+        <td>${str.state}</td>
+        <td>${str.tel}</td>
+        <td>${str.zip}</td>
+      </tr>
+    `
       )
       .join("");
-    tableBody.innerHTML = ren;
+    if (append) {
+      tableBody.innerHTML += rendered;
+    } else {
+      tableBody.innerHTML = rendered;
+    }
   };
 
-  const sortedData = sortData(data, currentSortType);
-  const currentPartData = sortedData.slice(
-    currentStep,
-    currentStep + paginateStep
-  );
+  const currentPartData = data.slice(currentStep, currentStep + paginateStep);
   displayData(currentPartData);
 
-  const btn = document.querySelector("button");
   btn.addEventListener("click", () => {
-    currentStep += 50;
-    displayData(sortedData.slice(currentStep, currentStep + paginateStep));
+    currentStep += paginateStep;
+    displayData(data.slice(currentStep, currentStep + paginateStep), true);
+    console.log(currentStep);
   });
 
   document.querySelectorAll(".sort").forEach((sortBtn) => {
     sortBtn.addEventListener("click", (event) => {
-      const type = event.target.parentNode.innerText
-        .split(" ")[0]
-        .toLowerCase();
-      if (currentSortType === type) {
+      const type = event.target.parentNode.innerText.split(" ")[0];
+      if (currentSortColumn === type) {
         currentSortOrder = -currentSortOrder;
       } else {
-        currentSortType = type;
+        currentSortColumn = type;
         currentSortOrder = 1;
       }
-
-      const sortedData = sortData(data, currentSortType);
-      const currentPartData = sortedData.slice(0, currentStep + paginateStep);
-      displayData(currentPartData);
+      const sortedData = sortData(
+        data.slice(0, currentStep),
+        currentSortColumn
+      );
+      displayData(sortedData);
     });
   });
 };
